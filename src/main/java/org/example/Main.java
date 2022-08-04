affa01369bdc269c650a3749b5b86c680ba057b7
Vsvolod <110387504+Vsvolod@users.noreply.github.com>
2022-08-04 15:33:35 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+import java.util.Scanner;
+
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name;
+        String city;
+        int age;
+        String hobby;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Say your name: ");
+        name = in.nextLine();
+        System.out.print("Write your hobby: ");
+        hobby = in.nextLine();
+        System.out.print("Where do you from? ");
+        city = in.nextLine();
+        System.out.print("How old are you? ");
+        age = in.nextInt();
+
+
+        System.out.printf("Your name %s \n You are from %s \n Your age is: %d \n Your hobby is %s", name,  city,  age, hobby);
+
     }
 }
